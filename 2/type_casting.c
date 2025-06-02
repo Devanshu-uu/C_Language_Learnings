5a2edd620939575139b6d04683e036c285769c80
Devanshu <devanshumohriya@gmail.com>
2025-06-02 22:45:56 +0530

    new

---
+++ 2/type_casting.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int n=2;
+    int m=2.3;
+    n=(int)m;
+    printf("m is : %d",m);
+    return 0;
+}
---
+++ 2/type_conversion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    float a=9.0;
+    int b=2;
+    float c = a/b;
+    int d=3.3; // Wrong 
+    printf("This is answer %f\n",c);
+    printf("This is d is : %d",d);
+    return 0;
+}
