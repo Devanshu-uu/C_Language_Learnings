8a2f5930700a53f0ce8bc19f952579b9182b45d2
Devanshu <devanshumohriya@gmail.com>
2025-07-02 23:33:13 +0530

    new

--- Functions_&_Recurtion_practice/07_problem.c
+++ Functions_&_Recurtion_practice/07_problem.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
-char pattern(n);
-char pattern(n){
-    printf("%c", (n-2) \n , n )
-}
+
 
 int main(){
+
+    int n;
+    printf("Enter a number : " );
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2*i+1 ; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+    }
     
     return 0;
 }
